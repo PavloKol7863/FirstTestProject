88791eb304ade099a94f283fcccd5e341f894460
Pavlo Koldun <pavlo123koldun@gmail.com>
2020-12-18 18:46:44 +0200

    Created income list

--- app/src/main/java/ua/application/statisticsapplication/IncomeListFragment.java
+++ app/src/main/java/ua/application/statisticsapplication/IncomeListFragment.java
@@ -1,6 +1,40 @@
 package ua.application.statisticsapplication;
 
+import android.os.Bundle;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
 import androidx.fragment.app.ListFragment;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class IncomeListFragment extends ListFragment {
+    AppDatabase db;
+    IncomeDao inDao;
+    List<Income> incomeList;
+    ArrayAdapter<String> adapter;
+    ArrayList<String> arrayList;
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        db = App.getInstance().getDatabase();
+
+        inDao = db.incomeDao();
+        incomeList = inDao.getAll();
+
+        createOutput();
+        adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, arrayList);
+        if (incomeList.size() != 0)
+            setListAdapter(adapter);
+        else
+            Toast.makeText(getContext(), "Don't found data", Toast.LENGTH_SHORT).show();
+    }
+    public void createOutput(){
+        arrayList = new ArrayList<>();
+        for (Income in:incomeList){
+            arrayList.add(in.getId()+"\t|"+in.getDat()+"\t|"+in.getNumLiter()+"\t|"+in.getSumm());
+        }
+    }
 }
