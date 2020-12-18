a2e59f40b147651292af0342b04f058d5c3fb585
Pavlo Koldun <pavlo123koldun@gmail.com>
2020-12-18 10:43:16 +0200

    Created two activity for two screen

---
+++ app/src/main/java/ua/application/statisticsapplication/CostsActivity.java
@@ -0,0 +1,14 @@
+package ua.application.statisticsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class CostsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.costs);
+    }
+}
---
+++ app/src/main/java/ua/application/statisticsapplication/IncomeActivity.java
@@ -0,0 +1,14 @@
+package ua.application.statisticsapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IncomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.income);
+    }
+}
